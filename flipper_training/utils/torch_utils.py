d16d636ac74aed556c3878eb9834b1e111eb66d1
David Korcak <david.korcak@gmail.com>
2025-03-27 13:18:11 +0100

    Adjust engine testing notebook to new interface

--- flipper_training/utils/torch_utils.py
+++ flipper_training/utils/torch_utils.py
@@ -8,6 +8,7 @@ def set_device(device: str) -> torch.device:
     if "cuda" in device and torch.cuda.is_available():
         torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True
+        torch.set_float32_matmul_precision("high")
         return torch.device("cuda")
     elif device == "mps" and torch.mps.is_available():
         return torch.device("mps")
